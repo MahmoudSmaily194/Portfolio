6ef51b6019f0077f2ab08fb11ac1c7ffb1cfa85a
Mahmoud <your@email.com>
2026-01-09 14:59:31 +0200

    second commit

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./nav.css";
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="Navbar">
         <div className="logo_con">
           <div className="logo">
-            <span className="material-symbols-outlined icon">terminal</span>
+            <span className="material-symbols-outlined">terminal</span>
           </div>
           <span>
             Mahmoud<span className="brand-dot">.</span>
@@ -16,8 +18,41 @@ const Nav = () => {
           <a>Work</a>
           <a>Stack</a>
         </nav>
-        <div className="resume"><span>Resume</span><span className="material-symbols-outlined">download</span></div>
+        <div className="resume">
+          <span>Resume</span>
+          <span className="material-symbols-outlined">download</span>
+        </div>
+
+        <div onClick={() => setIsOpen(!isOpen)} className="logo menu-icon">
+          {isOpen ? (
+            <span className="material-symbols-outlined">close</span>
+          ) : (
+            <span className="material-symbols-outlined icon">menu_open</span>
+          )}
+        </div>
       </div>
+      {isOpen && (
+        <div className="mobile-menu">
+          <div className="mobile-menu-link">
+            <span className="material-symbols-outlined">grid_view</span>
+            <a href="#">Body</a>
+          </div>
+          <div className="mobile-menu-link">
+            <span className="material-symbols-outlined">person_search</span>
+            <a href="#">Work</a>
+          </div>
+          <div className="mobile-menu-link">
+            <span className="material-symbols-outlined">database</span>
+            <a href="#">Stack</a>
+          </div>
+          <div className="mobile-menu-link">
+            <span className="material-symbols-outlined">
+              download_for_offline
+            </span>
+            <a href="#contact">Resume</a>
+          </div>
+        </div>
+      )}
     </>
   );
 };
